6cd48850449a64cf91e3c708c7ebdf274a962d7b
Nayan313 <nayansukhadiya31@gmail.com>
2024-09-06 16:45:48 +0530

    update the task 11

--- 11-oprator-control-flow/script.js
+++ 11-oprator-control-flow/script.js
@@ -1,3 +1,5 @@
+// conditional statement 
+
 let age = 23;
 
 if(age <= 12){
@@ -12,3 +14,29 @@ else if(age > 18 && age <= 90){
 else{
     console.log("enter valid age")
 }
+
+// print 1 to 10 using the for loop
+
+for(let i=1; i<=10; i++){
+    console.log(i)
+}
+
+// switch case
+function signal(signal) {
+    switch(signal) {
+        case 'green':
+            console.log("You can go");
+            break;
+        case 'yellow':
+            console.log("Get ready to go");
+            break;
+        case 'red':
+            console.log("You should stop");
+            break;
+        default:
+            console.log("Wait for the signal");
+            break;
+    }
+}
+
+signal('green');
